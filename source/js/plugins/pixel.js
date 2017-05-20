ba32f6b780c64436e9a55a172b1570dd9485a962
Zeyad Saleh <zeyadhazemsaleh@gmail.com>
2017-05-20 14:35:58 -0400

    Supported opacity control for multiple layers individually

--- source/js/plugins/pixel.js
+++ source/js/plugins/pixel.js
@@ -20,21 +20,23 @@ export default class PixelPlugin
 
     // Subscribes to VisibleTilesDidLoad event to start drawing highlights.
     // Takes an array of highlighted objects
-    activatePlugin(layer)
+    activatePlugin(layers)
     {
-        this.createPluginElements(layer);
-        let handle = this.subscribeToEvent(layer);
+        let handle = this.subscribeToEvent(layers);
         this.core.getSettings().renderer._paint();  // Repaint the tiles to retrigger VisibleTilesDidLoad
         this.activated = true;
+        layers.forEach((layer) => {
+            this.createPluginElements(layer);
+        });
         return handle;
     }
 
-    deactivatePlugin()
+    deactivatePlugin(layer)
     {
         Diva.Events.unsubscribe(this.handle);
         this.core.getSettings().renderer._paint(); // Repaint the tiles to make the highlights disappear off the page
         this.activated = false;
-        this.destroyPluginElements();
+        this.destroyPluginElements(layer);
     }
 
     createPluginElements(layer)
@@ -44,7 +46,7 @@ export default class PixelPlugin
         x.setAttribute("type", "range");
         x.setAttribute('max', 100);
         x.setAttribute('min', 0);
-        x.setAttribute('value', this.opacity*100);
+        x.setAttribute('value', layer.opacity*100);
         global.document.body.appendChild(x);
 
         var rangeInput = document.getElementById("layer " + layer.layerType);
@@ -63,75 +65,79 @@ export default class PixelPlugin
         global.document.body.removeChild(rangeInput);
     }
 
-    subscribeToEvent(layer){
+    subscribeToEvent(layers){
         let instance = this;
 
         let handle = Diva.Events.subscribe('VisibleTilesDidLoad', function(args)
         {
-            instance.drawHighlights(layer, args);
+            instance.drawHighlights(layers, args);
         });
         return handle;
     }
 
-    drawHighlights(layer, args)
+    drawHighlights(layers, args)
     {
         var pageIndex = args[0],
             zoomLevel = args[1];
-        var highlights = layer.highlights;
 
-        highlights.forEach((highlighted) =>
-            {
-                let opacity = layer.opacity;
-                let renderer = this.core.getSettings().renderer;
-                let scaleRatio = Math.pow(2,zoomLevel);
-
-                const viewportPaddingX = Math.max(0, (renderer._viewport.width - renderer.layout.dimensions.width) / 2);
-                const viewportPaddingY = Math.max(0, (renderer._viewport.height - renderer.layout.dimensions.height) / 2);
-
-                // The following absolute values are experimental values to highlight the square on the first page of Salzinnes, CDN-Hsmu M2149.L4
-                // The relative values are used to scale the highlights according to the zoom level on the page itself
-                let absoluteRectOriginX = highlighted.relativeRectOriginX * scaleRatio;
-                let absoluteRectOriginY = highlighted.relativeRectOriginY * scaleRatio;
-                let absoluteRectWidth = highlighted.relativeRectWidth * scaleRatio;
-                let absoluteRectHeight = highlighted.relativeRectHeight * scaleRatio;
-
-                // This indicates the page on top of which the highlights are supposed to be drawn
-                let highlightPageIndex = highlighted.pageIndex;
-
-                if (pageIndex === highlightPageIndex)
+        layers.forEach((layer) => {
+            var highlights = layer.highlights;
+            
+            highlights.forEach((highlighted) =>
                 {
-                    // Calculates where the highlights should be drawn as a function of the whole webpage coordinates
-                    // (to make it look like it is on top of a page in Diva)
-                    let highlightXOffset = renderer._getImageOffset(pageIndex).left - renderer._viewport.left + viewportPaddingX + absoluteRectOriginX;
-                    let highlightYOffset = renderer._getImageOffset(pageIndex).top - renderer._viewport.top + viewportPaddingY + absoluteRectOriginY;
+                    let opacity = layer.opacity;
+                    let renderer = this.core.getSettings().renderer;
+                    let scaleRatio = Math.pow(2,zoomLevel);
 
-                    //Draw the rectangle
-                    let rgba = null;
-                    switch (layer.layerType)
+                    const viewportPaddingX = Math.max(0, (renderer._viewport.width - renderer.layout.dimensions.width) / 2);
+                    const viewportPaddingY = Math.max(0, (renderer._viewport.height - renderer.layout.dimensions.height) / 2);
+
+                    // The following absolute values are experimental values to highlight the square on the first page of Salzinnes, CDN-Hsmu M2149.L4
+                    // The relative values are used to scale the highlights according to the zoom level on the page itself
+                    let absoluteRectOriginX = highlighted.relativeRectOriginX * scaleRatio;
+                    let absoluteRectOriginY = highlighted.relativeRectOriginY * scaleRatio;
+                    let absoluteRectWidth = highlighted.relativeRectWidth * scaleRatio;
+                    let absoluteRectHeight = highlighted.relativeRectHeight * scaleRatio;
+
+                    // This indicates the page on top of which the highlights are supposed to be drawn
+                    let highlightPageIndex = highlighted.pageIndex;
+
+                    if (pageIndex === highlightPageIndex)
                     {
-                        case 0:
-                            rgba = "rgba(51, 102, 255, " + opacity + ")";
-                            break;
-                        case 1:
-                            rgba = "rgba(255, 51, 102, " + opacity + ")";
-                            break;
-                        case 2:
-                            rgba = "rgba(255, 255, 10, " + opacity + ")";
-                            break;
-                        case 3:
-                            rgba = "rgba(10, 255, 10, " + opacity + ")";
-                            break;
-                        case 4:
-                            rgba = "rgba(120, 0, 120, " + opacity + ")";
-                            break;
-                        default:
-                            rgba = "rgba(255, 0, 0, " + opacity + ")";
+                        // Calculates where the highlights should be drawn as a function of the whole webpage coordinates
+                        // (to make it look like it is on top of a page in Diva)
+                        let highlightXOffset = renderer._getImageOffset(pageIndex).left - renderer._viewport.left + viewportPaddingX + absoluteRectOriginX;
+                        let highlightYOffset = renderer._getImageOffset(pageIndex).top - renderer._viewport.top + viewportPaddingY + absoluteRectOriginY;
+
+                        //Draw the rectangle
+                        let rgba = null;
+                        switch (layer.layerType)
+                        {
+                            case 0:
+                                rgba = "rgba(51, 102, 255, " + opacity + ")";
+                                break;
+                            case 1:
+                                rgba = "rgba(255, 51, 102, " + opacity + ")";
+                                break;
+                            case 2:
+                                rgba = "rgba(255, 255, 10, " + opacity + ")";
+                                break;
+                            case 3:
+                                rgba = "rgba(10, 255, 10, " + opacity + ")";
+                                break;
+                            case 4:
+                                rgba = "rgba(120, 0, 120, " + opacity + ")";
+                                break;
+                            default:
+                                rgba = "rgba(255, 0, 0, " + opacity + ")";
+                        }
+                        renderer._ctx.fillStyle = rgba;
+                        renderer._ctx.fillRect(highlightXOffset, highlightYOffset,absoluteRectWidth,absoluteRectHeight);
                     }
-                    renderer._ctx.fillStyle = rgba;
-                    renderer._ctx.fillRect(highlightXOffset, highlightYOffset,absoluteRectWidth,absoluteRectHeight);
                 }
-            }
-        )
+            )
+
+        })
     }
 
     /**
@@ -140,25 +146,28 @@ export default class PixelPlugin
      **/
     handleClick (event, settings, publicInstance, pageIndex)
     {
+        // Create the array of highlights to pass to drawHighlights function
+        let highlight1 = new HighlightArea(23, 42, 24, 24, 0);
+        let highlight2 = new HighlightArea(48, 50, 57, 5, 0);
+        let highlight3 = new HighlightArea(75, 80, 30, 10, 0);
+        let highlight4 = new HighlightArea(21, 77, 12, 13.5, 0);
+        let highlight5 = new HighlightArea(50, 120, 50, 10, 0);
+        let highlight6 = new HighlightArea(30, 180, 60, 20, 0);
+        let highlighted1 = [highlight1, highlight2];
+        let highlighted2 = [highlight3, highlight4];
+        let highlighted3 = [highlight5, highlight6];
+        let layer1 = new Layer(0,0.3, highlighted1);
+        let layer2 = new Layer(1,0.5, highlighted2);
+        let layer3 = new Layer(2,0.3, highlighted3);
+        let layers = [layer1, layer2, layer3];
+
         if (!this.activated)
         {
-            // Create the array of highlights to pass to drawHighlights function
-            let highlight1 = new HighlightArea(23, 42, 24, 24, 0);
-            let highlight2 = new HighlightArea(48, 50, 57, 5, 0);
-            let highlight3 = new HighlightArea(75, 80, 30, 10, 0);
-            let highlight4 = new HighlightArea(21, 77, 12, 13.5, 0);
-            let highlight5 = new HighlightArea(50, 120, 50, 10, 0);
-            let highlight6 = new HighlightArea(30, 180, 60, 20, 0);
-            let highlighted = [highlight1, highlight2, highlight3, highlight4, highlight5, highlight6];
-            let layer = new Layer(0,0.3, highlighted)
-
-            console.log("From activated", layer.highlights);
-
-            this.handle = this.activatePlugin(layer);
+            this.handle = this.activatePlugin(layers);
         }
         else
         {
-            this.deactivatePlugin();
+            this.deactivatePlugin(layers);
         }
     }
 
